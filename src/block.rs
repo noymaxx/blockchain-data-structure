b75bf49a353883377c538135e7779b144e5d3ebb
Hugo Noyma <hugo.povoleri@sou.inteli.edu.br>
2024-08-22 15:25:05 -0300

    feat: miner algorithm

--- src/block.rs
+++ src/block.rs
@@ -1,3 +1,4 @@
+use core::hash;
 use std::fmt::{ self, Debug, Formatter };
 use super::*;
 
@@ -8,15 +9,17 @@ pub struct Block {
     pub prev_block_hash: BlockHash,
     pub nonce: u64,
     pub payload: String,
+    pub difficulty: u128,
 }
 
 impl Debug for Block {
     fn fmt (&self, f: &mut Formatter) -> fmt::Result {
-        write!(f, "Block[{}]: {} at: {} with: {}",
+        write!(f, "Block[{}]: {} at: {} with: {} nonce: {}",
             &self.index,
             &hex::encode(&self.hash),
             &self.timestamp,
-            &self.payload
+            &self.payload,
+            &self.nonce,
         )
     }
 }
@@ -27,7 +30,9 @@ impl Block {
         timestamp: u128,
         prev_block_hash: BlockHash,
         nonce: u64,
-        payload: String
+        payload: String,
+        difficulty: u128
+
     ) -> Self {
         Block {
             index,
@@ -35,7 +40,19 @@ impl Block {
             hash: vec![0; 32],
             prev_block_hash,
             nonce,
-            payload
+            payload,
+            difficulty,
+        }
+    }
+
+    pub fn mine (&mut self) {
+        for nonce_attempt in 0..(u64::max_value()) {
+            self.nonce = nonce_attempt;
+            let hash = self.hash();
+            if check_difficulty(&hash, self.difficulty) {
+                self.hash = hash;
+                return;
+            }
         }
     }
 }   
@@ -53,3 +70,9 @@ impl Hashable for Block {
         bytes
     }
 }
+
+pub fn check_difficulty (hash  : &BlockHash, difficulty: u128) -> bool {
+    let hash_as_u128 = difficulty_bytes_as_u128(hash);
+    hash_as_u128 < difficulty
+}
+
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,22 @@
 use blockchainlib::*;
 
 fn main() {
-    let block = Block::new(13, now(), vec![0; 32], 0, "Genesis block!".to_owned());
+    let mut block = Block::new(0, 0, vec![0; 32], 0, "Genesis block!".to_owned(), 0x0000ffffffffffffffffffffffffffff);
 
     println!("{:?}", &block);
 
     let h = block.hash();
     println!("{:?}", &h);
+
+    block.hash = h;
+
+    println!("{:?}", &block);
+
+    block.hash = block.hash();
+
+    println!("{:?}", &block);
+
+    block.mine();
+
+    println!("{:?}", &block);
 }
