4c921d91fecbabe1982e14182da311cbcd242ad2
Hugo Noyma <hugo.povoleri@sou.inteli.edu.br>
2024-08-21 23:46:19 -0300

    feat: sha256 hash

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+use blockchainlib::*;
+
 fn main() {
-    println!("Hello, world!");
+    let block = Block::new(13, now(), vec![0; 32], 0, "Genesis block!".to_owned());
+
+    println!("{:?}", &block);
+
+    let h = block.hash();
+    println!("{:?}", &h);
 }
